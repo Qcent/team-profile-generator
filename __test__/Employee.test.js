85e36fbe1d26908a16870b761cb5de65e44793e3
Dave Quinn <dquinn8@cogeco.ca>
2021-10-21 20:25:34 -0400

    created en employee object that passed the test

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -7,13 +7,3 @@ test('creates an Employee object', () => {
     expect(worker.employeeId).toEqual(expect.any(Number));
     expect(worker.email).toEqual(expect.any(String));
 });
-
-/*
-test('creates a random potion object', () => {
-    const potion = new Potion();
-
-    expect(potion.name).toEqual(expect.any(String));
-    expect(potion.name.length).toBeGreaterThan(0);
-    expect(potion.value).toEqual(expect.any(Number));
-});
-*/
